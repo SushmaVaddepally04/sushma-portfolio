2faa3c6205d14f9f64092733e49f4a0e4b4c5133
Sushma Vaddepally <vsushma0124@gmail.com>
2025-08-01 22:45:39 +0530

    url

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/SushmaVaddepally04/">
+    <BrowserRouter basename="/SushmaVaddepally04">
       <App />
     </BrowserRouter>
   </StrictMode>,
